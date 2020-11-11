59397a22fd0a553e3492034f418ede8106e775e8
SwaitzJoses <68174417+SwaitzJoses@users.noreply.github.com>
2020-11-11 08:00:43 +0530

    Add files via upload

---
+++ titleCase.js
@@ -0,0 +1,11 @@
+var arr= "i am iron man!";
+function toTitleCase(str) {
+    return str.replace(
+      /\w\S*/g,
+      function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      }
+    );
+  }
+
+  console.log(toTitleCase(arr));
